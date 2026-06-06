b6519b8533dc4b3b763762362bf2c49998c0d6ca
claudiavinci <claudiavinci1@icloud.com>
2026-06-06 12:05:24 +0200

    pipeline da testare

---
+++ pipeline_mlkem.c
@@ -0,0 +1,118 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <time.h>
+#include <omp.h>
+#include "api.h"
+
+#define N_JOBS 100000
+// Definisco le costanti di successo e fallimento (seguendo la convenzione di PQClean)
+#define KEM_FAIL 1
+#define KEM_SUCCESS 0
+
+/*
+Kem job struct: contains all the necessary data for a single KEM operation, including: 
+- key generation, 
+- encapsulation,
+- decapsulation 
+*/
+typedef struct {
+    uint8_t pk[PQCLEAN_MLKEM768_CLEAN_CRYPTO_PUBLICKEYBYTES];
+    uint8_t sk[PQCLEAN_MLKEM768_CLEAN_CRYPTO_SECRETKEYBYTES];
+    uint8_t ct[PQCLEAN_MLKEM768_CLEAN_CRYPTO_CIPHERTEXTBYTES];
+    uint8_t ss_enc[PQCLEAN_MLKEM768_CLEAN_CRYPTO_BYTES];
+    uint8_t ss_dec[PQCLEAN_MLKEM768_CLEAN_CRYPTO_BYTES];
+    int status;
+} kem_job;
+
+// Pipeline stages definition
+static inline int keygen_stage(kem_job *job){
+    return PQCLEAN_MLKEM768_CLEAN_crypto_kem_keypair(job->pk, job->sk);
+}
+
+static inline int enc_stage(kem_job *job){
+    return PQCLEAN_MLKEM768_CLEAN_crypto_kem_enc(job->ct, job->ss_enc, job->pk);
+}
+
+static inline int dec_stage(kem_job *job){
+    return PQCLEAN_MLKEM768_CLEAN_crypto_kem_dec(job->ss_dec, job->ct, job->sk);
+}
+
+static inline int check_stage(kem_job *job){
+    return memcmp(job->ss_enc, job->ss_dec, PQCLEAN_MLKEM768_CLEAN_CRYPTO_BYTES) == 0;
+}
+
+int main(int argc, char *argv[]) {
+    printf("Pipeline execution starting...\n");
+
+    int global_success = 0;
+    struct timespec t0, t1;
+    static kem_job jobs[N_JOBS];
+
+    timespec_get(&t0, TIME_UTC); // Prendo il tempo di inizio
+
+    #pragma omp parallel
+    {
+        #pragma omp single
+        {
+            for(int i=0; i < N_JOBS; i++){
+                jobs[i].status = KEM_SUCCESS;
+
+                // ---------- KEYGEN STAGE ----------
+                #pragma omp task depend(out: jobs[i].pk, jobs[i].sk)
+                {
+                    if (keygen_stage(&jobs[i]) != KEM_SUCCESS){
+                        jobs[i].status = KEM_FAIL;
+                        printf("Key pair generation failed for job %d\n", i);
+                    }
+                }
+
+                // ---------- ENCAPSULATION STAGE ----------
+                #pragma omp task depend(in: jobs[i].pk, jobs[i].sk) depend(out: jobs[i].ct, jobs[i].ss_enc)
+                {
+                    if (jobs[i].status == KEM_SUCCESS) {
+                        if (enc_stage(&jobs[i]) != KEM_SUCCESS){
+                            jobs[i].status = KEM_FAIL;
+                            printf("Encapsulation failed for job %d\n", i);
+                        }
+                    }
+                }
+
+                // ---------- DECAPSULATION STAGE ----------
+                #pragma omp task depend(in: jobs[i].ct, jobs[i].sk) depend(out: jobs[i].ss_dec)
+                {
+                    if (jobs[i].status == KEM_SUCCESS) {
+                        if (dec_stage(&jobs[i]) != KEM_SUCCESS){
+                            jobs[i].status = KEM_FAIL;
+                            printf("Decapsulation failed for job %d\n", i);
+                        }
+                    }
+                }
+
+                // ---------- CHECK STAGE ----------
+                #pragma omp task depend(in: jobs[i].ss_enc, jobs[i].ss_dec)
+                {
+                    if (jobs[i].status == KEM_SUCCESS){
+                        if (!check_stage(&jobs[i])){
+                            jobs[i].status = KEM_FAIL;
+                            printf("Shared secrets do not match for job %d. Test failed.\n", i);
+                        } else {
+                            #pragma omp atomic
+                            global_success++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    timespec_get(&t1, TIME_UTC); // Prendo il tempo di fine
+    double elapsed_time = (t1.tv_sec - t0.tv_sec) + (t1.tv_nsec - t0.tv_nsec) / 1e9;
+    printf("\n=== PIPELINE EXECUTION COMPLETED===\n");
+    printf("Jobs: %d\n", N_JOBS);
+    printf("Success: %d / %d \n", global_success, N_JOBS);
+    printf("Total time: %f sec\n", elapsed_time);
+    printf("=====================================\n");
+
+    return 0;
+}
