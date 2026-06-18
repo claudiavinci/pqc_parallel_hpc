5f9fb52373066cf79e9b5425af97dbf607b9030e
claudiavinci <claudiavinci1@icloud.com>
2026-06-18 15:00:41 +0200

    printf nell'init mpi

--- pipeline/pipeline_mpi_mlkem.c
+++ pipeline/pipeline_mpi_mlkem.c
@@ -10,8 +10,14 @@
 
 int main(int argc, char *argv[]) {
 
+    printf("START MAIN\n");
+    fflush(stdout);
+
     MPI_Init(&argc, &argv);
     
+    printf("AFTER MPI_INIT\n");
+    fflush(stdout);
+
     int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
